1d7bd9505c9175beb90ef35cf4a2dba82b29397b
John J Schultz <johnmail@bitsofgenius.com>
2020-02-15 22:59:16 -0500

    fix path 2

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -ev
 
-dotnet build -c $BUILD_CONFIG ./BOG.SwissArmyKnife.sln
+dotnet build -c $BUILD_CONFIG ./src/BOG.SwissArmyKnife.sln
 
 if [ $? -eq 0 ]
 then
 		if [ "$BUILD_CONFIG" = "debug" ]; 
 		then
-				dotnet test ./BOG.SwissArmyKnife.Test/BOG.SwissArmyKnife.Test.csproj -v normal --no-build
+				dotnet test ./src/BOG.SwissArmyKnife.Test/BOG.SwissArmyKnife.Test.csproj -v normal --no-build
 		else
 				echo Skip testing on non-debug build
 		fi
@@ -17,7 +17,7 @@ if [ $? -eq 0 ]
 then
 		if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; 
 		then
-				dotnet nuget push ./BOG.SwissArmyKnife/bin/$BUILD_CONFIG/BOG.SwissArmyKnife.*.nupkg --api-key $NUGET_API_KEY --source $NUGET_SOURCE --skip-duplicate
+				dotnet nuget push ./src/BOG.SwissArmyKnife/bin/$BUILD_CONFIG/BOG.SwissArmyKnife.*.nupkg --api-key $NUGET_API_KEY --source $NUGET_SOURCE --skip-duplicate
 		else
 				echo Skip nuget for non-master build
 		fi
