ce8a8a6f46e27c4864b2c20b26cc6936a3043a8a
John J Schultz <johnmail@bitsofgenius.com>
2020-02-16 01:19:00 -0500

    syntax fix

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,9 @@ set -ev
 
 pwd
 
-echo $NUGET_API_KEY == (hidden)
+echo $NUGET_API_KEY == hidden
 echo $NUGET_SOURCE == ${NUGET_SOURCE} 
-echo $TRAVIS_PULL_REQUEST == ${$TRAVIS_PULL_REQUEST}
-
-dotnet build -c $BUILD_CONFIG ./src/BOG.SwissArmyKnife.sln
+echo $TRAVIS_PULL_REQUEST == ${TRAVIS_PULL_REQUEST}dotnet build -c $BUILD_CONFIG ./src/BOG.SwissArmyKnife.sln
 
 if [ $? -eq 0 ]
 then
