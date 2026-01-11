a1030c3f6d3fcbe3b5dad3417081db5584d4caaa
John J Schultz <rambotech@users.noreply.github.com>
2026-01-11 15:21:23 -0500

    fix issues with demo calls

--- src/BOG.SwissArmyKnife.Demo/CipherUtilityDemo.cs
+++ src/BOG.SwissArmyKnife.Demo/CipherUtilityDemo.cs
@@ -7,30 +7,29 @@ namespace BOG.SwissArmyKnife.Demo
 {
     public class CipherUtilityDemo
     {
-        List<string> AlgorithmClassList = new();
+        private enum CryptographyAlgorithm : int
+        {
+            Aes = 0,
+            DES = 1,
+            TripleDES = 2
+        }
 
         public CipherUtilityDemo()
         {
-            HydrateSymmetricAlgorithmClasses();
-            AlgorithmClassList.Add("System.Security.Cryptography.AesManaged");
-            AlgorithmClassList.Add("System.Security.Cryptography.TripleDESCryptoServiceProvider");
-            AlgorithmClassList.Add("System.Security.Cryptography.RijndaelManaged");
         }
 
-        private void HydrateSymmetricAlgorithmClasses()
+        private SymmetricAlgorithm HydrateSymmetricAlgorithmClass(CryptographyAlgorithm alg)
         {
-            var type = typeof(SymmetricAlgorithm);
-            foreach (Type thisType in AppDomain.CurrentDomain.GetAssemblies()
-                .SelectMany(s => s.GetTypes())
-                .Where(p => type.IsAssignableFrom(p)).Where(t => type.IsClass))
+            switch (alg)
             {
-                if (!thisType.IsAbstract && thisType.IsAnsiClass)
-                {
-                    if (thisType.Name.Contains("Managed") || thisType.Name.Contains("Provider"))
-                    {
-                        AlgorithmClassList.Add(thisType.ToString());
-                    }
-                }
+                case CryptographyAlgorithm.Aes:
+                    return Aes.Create();
+                case CryptographyAlgorithm.DES:
+                    return DES.Create();
+                case CryptographyAlgorithm.TripleDES:
+                    return TripleDES.Create();
+                default:
+                    throw new NotSupportedException($"Symmetric algorithm {alg} is not supported.");
             }
         }
 
@@ -40,28 +39,16 @@ namespace BOG.SwissArmyKnife.Demo
             var password = "YeneM1lka$";
             var salt = string.Empty;
 
-            foreach (var algorithm in AlgorithmClassList)
+            foreach (var alg in Enum.GetValues<CryptographyAlgorithm>())
             {
-#if FALSE    // as of this time, reflection on algorithm classes is not working in netstandard20 dotnetcore
-                var cipherType = Type.GetType(algorithm);
-                var cipher = new CipherUtility((System.Security.Cryptography.SymmetricAlgorithm)Activator.CreateInstance(cipherType));
-#else
-                CipherUtility cipher = null;
-                if (algorithm == "System.Security.Cryptography.AesManaged")
-                {
-                    cipher = new CipherUtility(Aes.Create());
-                }
-                else if (algorithm == "System.Security.Cryptography.TripleDESCryptoServiceProvider")
-                {
-                    cipher = new CipherUtility(TripleDES.Create());
-                }
-#endif
+                var cryptoObj = HydrateSymmetricAlgorithmClass(alg);
+                var cipher = new CipherUtility(cryptoObj);
                 int pass = 1;
                 while (pass < 3)
                 {
                     Console.WriteLine("=======================================");
                     salt = DateTime.Now.ToString("fffssmm");
-                    Console.WriteLine($"Pass: {pass} of {algorithm}...");
+                    Console.WriteLine($"Pass: {pass} of {Enum.GetName<CryptographyAlgorithm>(alg)}...");
                     Console.WriteLine($"  raw: {raw}");
                     var encrypted = cipher.Encrypt(raw, password, salt, Base64FormattingOptions.InsertLineBreaks);
                     Console.WriteLine($"  encrypted: {encrypted}");
